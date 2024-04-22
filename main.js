b003500fd994075234a092e653a5ac4df43e3f80
Abdelrhman Qouay <qouay555@gmail.com>
2024-04-22 06:43:46 +0200

    added some new tricks

--- main.js
+++ main.js
@@ -3,13 +3,21 @@
 // ==================== Declare All Variables ====================
 const pass = document.getElementsByName('password')[0],
 rangePass = document.getElementsByName('rangePass')[0],
-rangeSpan = document.getElementById('numpass');
+rangeSpan = document.getElementById('numpass'),
+
+strongCheck = document.getElementById('strong'),
+SPcharCheck = document.getElementById('sp_chars'),
+ScharCheck = document.getElementById('sm_chars'),
+CcharCheck = document.getElementById('cap_chars'),
+numbersCheck = document.getElementById('numbers'),
+SPACEcharCheck = document.getElementById('space_char');
 
 
 const Schars = 'qwertyuiopasdfghjklzxcvbnm',
 Cchars = 'QWERTYUIOPASDFGHJKLZXCVBNM',
 nums = '0123456789',
-SPchars = `~!@#$%^&*_+-=?/\/`;
+SPchars = `~!@#$%^&*_+-=?/\/`,
+spaceChar = ' ';
 
 // ==================== Operations Used ====================
 rangePass.onchange = ()=> {
@@ -17,6 +25,14 @@ rangePass.onchange = ()=> {
     pass.setAttribute("maxlength", rangePass.value);
     pass.value = pass.value.substring(0, rangePass.value);
     console.log(pass.value.length);
+}
+
+strongCheck.onchange = ()=> {
+    if(strongCheck.checked) {
+        SPcharCheck.checked= true;ScharCheck.checked = true; CcharCheck.checked= true; numbersCheck.checked= true;SPACEcharCheck.checked= false;
+    }else {
+        SPcharCheck.checked= false;ScharCheck.checked = true; CcharCheck.checked= false; numbersCheck.checked= true;SPACEcharCheck.checked= false;
+    }
 }
 
 // ==================== All Functions Used ====================
@@ -41,14 +57,27 @@ function copyPass() {
 }
 
 function reGenerate() {
-    var tex = ``;
-    pass.value = tex;
+    console.log('re generate')
+    var text = generate_shuffle(pass.value);
+    pass.value = text;
 }
 
 // Generate and Shuffle Function v1.1 {use algorithm similar to the Fisher-Yates shuffle}
 // str >> string text to shuffle it | reqLen >> required length should generate on it | ... >> check the options one by one when generate
 function generate_shuffle(str, reqLen, checkSmall = true, checkCap = true, checkNums = true, checkSP = true) {
-    if(!str || !len) return reqLen;
-    let res= ``;
-    return res;
+    // if(!str || !reqLen) return reqLen;
+    let res= str.split('');
+    for(let i= 0; i < str.length; i++) {
+        // get a random number to shuffle it | contains the old character before anything
+        let randToShuffle = Math.floor(Math.random() * str.length),
+        oldChar = res[i];
+
+        // change the current char =to= random char
+        res[i] = res[randToShuffle];
+
+        // then make the random index[char] = the old char {that was the current}
+        res[randToShuffle] = oldChar;
+
+    }
+    return res.join('');
 }
